3ed020fa5f8401530d179faf5d24ba2d15c499b3
Roman Chernenko <chernenko.rv@gmail.com>
2025-08-23 00:01:18 +0300

    base support of NDVI

--- Sentinel2GridLayoutWorker.js
+++ Sentinel2GridLayoutWorker.js
@@ -61,7 +61,7 @@ class Sentinel2RgbDataLoader {
   #tiffCache = new Map();
   #stac = null;
   #abortControllers = new Map();
-  #tiffPool = new Pool();
+  _tiffPool = new Pool();
   #cellRgbCache = new QuickLRU({ maxSize: 1000 });
   #cellDates = new Map();
   #visibleCellKeys = new Set();
@@ -200,7 +200,7 @@ class Sentinel2RgbDataLoader {
   async getCellRgbImage(tiff, warpedImage, cellCoordsUtm, cellSize, signal) {
     const bbox = turf.bbox(turf.lineString(cellCoordsUtm) );
     const cellRGB = await withRetry(tiff.readRasters.bind(tiff))({
-      pool: this.#tiffPool,
+      pool: this._tiffPool,
       bbox: bbox,
       resX: (bbox[2] - bbox[0])/cellSize.x,
       resY: (bbox[3] - bbox[1])/cellSize.y,
@@ -281,7 +281,127 @@ class Sentinel2RgbDataLoader {
 }
 
 class Sentinel2NdviDataLoader extends Sentinel2RgbDataLoader {
+  async loadAndDrawTile(pkg, stacItems, warpedImage, signal) {
+    for (const stacItem of stacItems) {
+      if (signal.aborted) {
+        console.log("Abort after fetching stac item");
+        return;
+      }
+
+      const epsgCode = stacItem.properties["proj:epsg"];
+      const wgs84ToUTM = proj4("WGS84", `EPSG:${epsgCode}`);
+
+      const cellCoordsUtm = pkg.cellCoords.map(xy => wgs84ToUTM.forward([xy[0], xy[1]]));
+
+      const redBand = stacItem.assets.B04.href;
+      const nirBand = stacItem.assets.B08.href;
+
+      const redTiff = await this.openGeoTiffFile(redBand);
+      if (signal.aborted) {
+        console.log("Abort after opening red band geotiff file");
+        return;
+      }
+
+      const nirTiff = await this.openGeoTiffFile(nirBand);
+      if (signal.aborted) {
+        console.log("Abort after opening nir band geotiff file");
+        return;
+      }
+
+      await this.getCellRgbImage([redTiff, nirTiff], warpedImage, cellCoordsUtm, pkg.tileSize, signal);
+    }
+  }
   
+  async getCellRgbImage(tiff, warpedImage, cellCoordsUtm, cellSize, signal) {
+    const bbox = turf.bbox(turf.lineString(cellCoordsUtm) );
+    const cellRed = await withRetry(tiff[0].readRasters.bind(tiff[0]))({
+      pool: this._tiffPool,
+      bbox: bbox,
+      resX: (bbox[2] - bbox[0])/cellSize.x,
+      resY: (bbox[3] - bbox[1])/cellSize.y,
+      signal: signal,
+    });
+
+    const cellNir = await withRetry(tiff[1].readRasters.bind(tiff[1]))({
+      pool: this._tiffPool,
+      bbox: bbox,
+      resX: (bbox[2] - bbox[0])/cellSize.x,
+      resY: (bbox[3] - bbox[1])/cellSize.y,
+      signal: signal,
+    });
+
+    const cellRGB = this.calculateNdvi(cellRed, cellNir);
+    await this.warpCellImage(cellRGB, warpedImage, cellCoordsUtm, bbox, cellSize);
+  }
+
+  calculateNdvi(redRaster, nirRaster) {
+    const rgbRaster = {
+      width: redRaster.width,
+      height: redRaster.height,
+      0: new Array(redRaster[0].length).fill(0),
+      1: new Array(redRaster[0].length).fill(0),
+      2: new Array(redRaster[0].length).fill(0),
+    }
+
+    for (let i = 0; i < redRaster[0].length; i++) {
+      if (!redRaster[0][i] && !nirRaster[0][i])
+        continue
+
+      let ndvi = (nirRaster[0][i] - redRaster[0][i]) / (nirRaster[0][i] + redRaster[0][i]);
+      ndvi = Math.max(0, Math.min(ndvi, 1));
+      const rgb = this.ndviToRGB(ndvi);
+      rgbRaster[0][i] = rgb[0];
+      rgbRaster[1][i] = rgb[1];
+      rgbRaster[2][i] = rgb[2];
+    }
+
+    return rgbRaster;
+  }
+
+  ndviToRGB(ndvi) {
+    // Define color stops: [ndvi, R, G, B]
+    const colormap = [
+      [0,     0xea, 0xea, 0xea],
+      [0.025, 0xff, 0xf9, 0xcc],
+      [0.05,  0xed, 0xe8, 0xb5],
+      [0.075, 0xdd, 0xd8, 0x9b],
+      [0.1,   0xcc, 0xc6, 0x82],
+      [0.125, 0xbc, 0xb7, 0x6b],
+      [0.15,  0xaf, 0xc1, 0x60],
+      [0.175, 0xa3, 0xcc, 0x59],
+      [0.2,   0x91, 0xbf, 0x51],
+      [0.25,  0x7f, 0xb2, 0x47],
+      [0.3,   0x70, 0xa3, 0x3f],
+      [0.35,  0x60, 0x96, 0x35],
+      [0.4,   0x4f, 0x89, 0x2d],
+      [0.45,  0x3f, 0x7c, 0x23],
+      [0.5,   0x30, 0x6d, 0x1c],
+      [0.55,  0x21, 0x60, 0x11],
+      [0.6,   0x0f, 0x54, 0x0a],
+      [1,     0x00, 0x44, 0x00],
+    ];
+
+    // Find surrounding stops
+    let lower = colormap[0];
+    let upper = colormap[colormap.length - 1];
+    for (let i = 0; i < colormap.length - 1; i++) {
+      if (ndvi >= colormap[i][0] && ndvi <= colormap[i + 1][0]) {
+        lower = colormap[i];
+        upper = colormap[i + 1];
+        break;
+      }
+    }
+
+    // Normalize position between stops
+    const t = (ndvi - lower[0]) / (upper[0] - lower[0]);
+
+    // Interpolate RGB
+    const r = Math.round(lower[1] + t * (upper[1] - lower[1]));
+    const g = Math.round(lower[2] + t * (upper[2] - lower[2]));
+    const b = Math.round(lower[3] + t * (upper[3] - lower[3]));
+
+    return [ r, g, b ];
+  }
 }
 
 const layerDataLoaders = new Map([
